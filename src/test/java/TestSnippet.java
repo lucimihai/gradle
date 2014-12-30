9461f553eccf7aecb063aa93d233b7bc8fd05a59
luk <luk@fedora.local>
2014-12-30 13:34:50 +0200

    sync

---
+++ src/test/java/TestSnippet.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by luk on 12/30/14.
+ */
+public class TestSnippet {
+
+    @Test
+    public void always_success(){
+        Assert.assertTrue(true);
+    }
+
+}
